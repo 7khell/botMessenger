908b853e4897edf7e770186d2184dcc8b4451f71
7khell <kristhian.edison@outlook.es>
2024-01-03 11:29:49 -0500

    first commit

---
+++ datos.js
@@ -0,0 +1,3 @@
+module.exports = {
+    base: 'Eres un representante del Archivo regional de Puno. Las personas te consultan sobre la existencia de algún documento en específico. En todos los casos el archivo está perdido.',
+  };
---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const OpenAI = require("openai");
+//conexion con OpenAi
+const openai = new OpenAI({
+  apiKey: "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"
+});
+
+
+
+
+const app = express().use(bodyParser.json());
+//token de api de messenger
+const PAGE_ACCESS_TOKEN = 'EAAExju54ngQBOZCotWJG8j1hayJm0yaGcrIUaRXbURZAgkREp3mBZCYfqQrX6Tg5arH0UquBZC8A2tm94QvRTE9RA5ZAK4vI8YJy1VZBLoCl087jZBQMVFGHrvzW6kURFZAk875FIq91NGtSArHi02DpI9uB4qNNOkPuiS8QgaSuf2kPfN4kVA9O2UKSs54Y5eje';
+
+//Purta de coneccion con la Api de Facebook
+app.post('/webhook', (req, res) =>{
+    console.log('POST: webhook');
+    const body = req.body;
+
+    if(body.object === 'page'){
+    
+        body.entry.forEach(entry => {
+            // recibir mensaje y id de mensaje
+            const webhookEvent = entry.messaging[0];
+            const sender_psid = webhookEvent.sender.id;
+
+            //validar que se este recibiendo Mensajes
+            if(webhookEvent.message){
+                handleMessage(sender_psid, webhookEvent.message);
+            }
+ 
+        });
+
+        res.status(200).send('EVENTO RECIBIDO');
+    }else{
+        res.sendStatus(404);
+    }
+});
+
+//Autentificacion con Facebook Api
+app.get('/webhook', (req, res) =>{
+    const VERIFY_TOKEN = '232lflkdlplvstrka24'; 
+
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if(mode && token){
+        if(mode === 'subscribe' && token === VERIFY_TOKEN){
+            console.log('Logeado con exito!');
+            res.status(200).send(challenge);
+        }else{
+            res.sendStatus(404);
+        }
+    }else{
+        res.sendStatus(404);
+    }
+});
+
+
+
+//Creacion de Mensajes
+function handleMessage(sender_psid, received_message) {
+    let response;
+    //funcion para conectar con openAI
+    const openFun=async()=>{
+        var data = require('./datos');
+        const chatCompletion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {"role": "system", "content": data.base},
+                {"role": "user", "content": received_message.text,}
+            ],
+            max_tokens:100
+          });
+          
+          
+          response = {
+            'text' : chatCompletion.choices[0].message.content
+         }
+    
+          callSendAPI(sender_psid, response)
+    }
+    
+
+    if (received_message.text) {
+        openFun();
+    }else if(received_message.attachments){
+        response = {'text': 'Solo puede comunicarse con nosotros mediante Texto'};
+    };
+}
+
+//Envio de datos mediante Api
+function callSendAPI(sender_psid, response) {
+    let requestBody = {
+        'recipient' : {
+            'id' : sender_psid
+        },
+        'message': response
+    }
+
+    request({
+        'uri' : 'https://graph.facebook.com/v18.0/me/messages',
+        'qs'  :{'access_token' : PAGE_ACCESS_TOKEN},
+        'method' : 'POST',
+        'json' : requestBody
+    }, (err, res, body)=>{
+         if(!err){
+            console.log('se Envio')
+         }else{
+            console.log('Fallo de envio')
+         }
+    })
+}
+
+
+//Mensaje de integracion
+app.get('/', (req, res) => {
+    res.status(200).send('Hola a mi bot!')
+})
+
+app.listen(3000, () =>{
+    console.log('Servidor iniciado...');
+});
